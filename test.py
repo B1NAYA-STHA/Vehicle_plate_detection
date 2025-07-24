a16b7d908612a3878bce391b65f6098ed9152cd3
Binaya shrestha <sthavicky555@gmail.com>
2025-07-24 19:53:43 +0545

    testing file

--- test.py
+++ test.py
@@ -1,45 +1,31 @@
 from ultralytics import YOLO
 import cv2 as cv
-import math
 import supervision as sv
 import numpy as np
+import easyocr
 
-def draw_label(frame, text, x, y, color=(0, 255, 0)):
-    font = cv.FONT_HERSHEY_SIMPLEX
-    scale = 0.6
-    thickness = 2
-    (w, h), _ = cv.getTextSize(text, font, scale, thickness)
-    cv.rectangle(frame, (x, y - h - 10), (x + w + 10, y), color, -1)  # Label background
-    cv.putText(frame, text, (x + 5, y - 5), font, scale, (0, 0, 0), thickness, cv.LINE_AA)  # Black text for contrast
+model = YOLO("Vehicle_plate_detection\model\\best.pt")
+img = cv.imread("Vehicle_plate_detection\photos\\1.jpg")
 
-model = YOLO("yolov8n.pt")
-# img = cv.imread("Vehicle_license_plate_detection\photos\\2.jpg")
-# result = model(img)
-# result[0].show()
 
-cap = cv.VideoCapture("Vehicle_license_plate_detection\\videos\\1.mp4")
-class_id = [2, 3, 5, 7]
-byte_tracker = sv.ByteTrack()
-while True:
-    ret, img = cap.read()
-    frame = cv.resize(img, (1280, 720))
-    result = model(frame)[0]
-    detections = sv.Detections.from_ultralytics(result) 
-    detections = detections[np.isin(detections.class_id, class_id)]
+result = model(img)
+reader = easyocr.Reader(['en'])
 
-    tracker = byte_tracker.update_with_detections(detections)
-    for detection in tracker:
-        if detection[4] != -1:
-            x1, y1, x2, y2 = detection[0].astype(int)
-            track_id = detection[4]
-            class_name = model.names[detection[3]]
-            label = f"{track_id}: {class_name}"
-            cv.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3, cv.LINE_AA)
-            draw_label(frame, label, x1, y1, (255, 0, 255))
-        
-    cv.imshow("car", frame)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+for r in result:
+    for box in r.boxes:
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        crop = img[y1:y2, x1:x2]
 
-cap.release
-cv.destroyAllWindows
+        ocr_results = reader.readtext(crop)
+        for (_, text, conf) in ocr_results:
+            print(f"Detected text: {text}, Confidence: {conf}")
+
+        # Draw result on image
+        cv.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        if ocr_results:
+            cv.putText(img, ocr_results[0][1], (x1, y1 - 10),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            
+cv.imshow('car', img)
+cv.waitKey(0)
+    
