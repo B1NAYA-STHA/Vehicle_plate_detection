f6a6508846f3b6a9cb20c3151c5ed2933a78da6b
Binaya shrestha <sthavicky555@gmail.com>
2025-07-24 19:56:51 +0545

    utilities file for video

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from .video_utils import draw_boxes
+from .save_utlis import video_writer, save_video
--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,8 +1,8 @@
 import cv2 as cv
-import math
 from .classes import classNames
 import supervision as sv
 import numpy as np
+from lp_detector import detect_lp
 
 class_id = [2, 3, 5, 7]
 byte_tracker = sv.ByteTrack()
@@ -36,18 +36,22 @@ def draw_label(frame, text, x, y, color=(0, 255, 0)):
 #                 draw_label(frame, label, x1, y1, (255, 0, 255))
 #     return frame   
 
-def draw_boxes(frame, result, model):
+def draw_boxes(frame, result, model, frame_count, skip_rate = 3):
     detections = sv.Detections.from_ultralytics(result[0]) 
     detections = detections[np.isin(detections.class_id, class_id)]
 
     tracker = byte_tracker.update_with_detections(detections)
-    for detection in detections:
-        if detection[4] != -1:
-            x1, y1, x2, y2 = detection[0].astype(int)
-            track_id = detection[4]
-            class_name = model.names[detection[3]]
-            label = f"{track_id}: {class_name}"
-            cv.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3, cv.LINE_AA)
-            draw_label(frame, label, x1, y1, (255, 0, 255))
+    for detection in tracker:
+        if detection[4] == -1:
+            continue
+        x1, y1, x2, y2 = detection[0].astype(int)
+        track_id = detection[4]
+        class_name = model.names[detection[3]]
+        label = f"{track_id}: {class_name}"
+        cv.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3, cv.LINE_AA)
+        draw_label(frame, label, x1, y1, (255, 0, 255))
 
+        if frame_count % skip_rate == 0:
+            frame = detect_lp(frame, (x1, y1, x2, y2))
+           
     return frame
