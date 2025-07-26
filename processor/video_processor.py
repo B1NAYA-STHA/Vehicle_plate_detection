8824c9a855018c55c1367f12256d12b7c015c12b
Binaya shrestha <sthavicky555@gmail.com>
2025-07-26 20:55:49 +0545

    get frame and result

--- processor/video_processor.py
+++ processor/video_processor.py
@@ -1,13 +1,16 @@
 import cv2 as cv
 
 def get_frame_and_result(model, video_url):
-    cap = cv.VideoCapture(video_url)
+    cap = cv.VideoCapture(video_url)  # Open video file or stream
+
     while True:
-        ret, img = cap.read()
-        if not ret:
+        ret, img = cap.read()  # Read a frame
+        if not ret:  
             break
-        frame = cv.resize(img, (1280, 720))
-        results = model(frame)
-        yield frame, results
-    
-    cap.release
+
+        frame = cv.resize(img, (1280, 720)) 
+        results = model(frame)  # Run model inference on the frame
+
+        yield frame, results  # Yield frame and model results as a generator
+
+    cap.release() 
