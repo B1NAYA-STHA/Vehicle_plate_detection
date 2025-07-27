e8e73b5125ca0a034b1e8af31740dd9ccca71ff7
Binaya shrestha <sthavicky555@gmail.com>
2025-07-27 19:36:16 +0545

    program for testing codes

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import cv2 as cv
 import numpy as np
+import pandas as pd
 from ultralytics import YOLO
 import easyocr
+import os
 
 model = YOLO("Vehicle_plate_detection/model/best.pt")
 img = cv.imread("Vehicle_plate_detection/photos/5.jpg")
 
 reader = easyocr.Reader(['en'])
+csv_file = "Vehicle_plate_detection\csv_files\info.csv"
 
+if not os.path.exists(csv_file):
+    df = pd.DataFrame(columns=["plate_bbox", "plate_num", "conf"])
+    df.to_csv(csv_file, index=False)
 # Run YOLO prediction
+
 results = model(img)[0]
 
 for r in results:
@@ -16,7 +23,12 @@ for r in results:
         x1, y1, x2, y2 = map(int, box.xyxy[0])
         crop = img[y1:y2, x1:x2]
         text = reader.readtext(crop)
-        print(len(text))
+        new_data = pd.DataFrame([{
+            "plate_bbox": (x1, y1, x2, y2),
+            "plate_num": text[0][1],
+            "conf": text[0][2]
+        }])
+        new_data.to_csv(csv_file, mode='a', header=False, index=False)
         print(f"Detected Text: {text[0][1]}")
 
 # Show the final image
